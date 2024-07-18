ddde3bdeaeb7ad8585041a1970ff03dfd804f64a
beastsenior <26155865+beastsenior@users.noreply.github.com>
2024-07-18 16:29:50 +0800

    1

--- signal_detector.py
+++ signal_detector.py
@@ -1,2 +1,8 @@
+import numpy as np
+
 class Signal_Detector():
-    pass
+
+    #martched filter
+    def MF(self, H, Rx_sig):
+        Tx_sig_hat = np.dot(np.conj(H).T , Rx_sig)
+        return Tx_sig_hat
